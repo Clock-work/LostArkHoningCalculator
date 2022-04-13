e06e406f4252c6b2c542ce5558b06b366e800231
Clock-work <clock-work@gmx.de>
2022-04-13 14:48:01 +0200

    - gear selection update

--- LostArkHoningCalculator/include/LostArkHoningCalculator/GearScoreSelection.h
+++ LostArkHoningCalculator/include/LostArkHoningCalculator/GearScoreSelection.h
@@ -6,7 +6,8 @@ static void printBestTargetGearScoreSelection()
 	if (HoningConfig::isIlvl1340Set)//todo: rely on calculation in the future 
 	{
 		std::cout << "For the ilvl 1340 set it's best to buy the weapon and 1 armour from abyss dungeon and 2 armour pieces from argos in the first week" << std::endl;
-		std::cout << "Then upgrade the weapon to +11, chest and leg armour to +8 and the other armour pieces to +17 to reach ilvl 1415" << std::endl;
+		std::cout << "Then for DPS upgrade the weapon to +11, gloves to +13 and other armour to +12 to reach ilvl 1400" << std::endl;
+		std::cout << "For 1415 go for weapon +11, armour +17,17,13,12,12" << std::endl;
 		std::cout << "For ilvl 1385 you can also do one more hard abyss dungeon and not do argos before you reach the level"
 			<< " and upgrade chest / leg to + 8, weapon / helmet to + 9 and gloves / shoulders to + 10" << std::endl << std::endl;
 	}
